e2a133858b14cd2b83eb08af0533eaf1355ea01e
ycchen <gruenen@gmail.com>
2019-12-29 22:07:41 +0100

    responsive topbar

--- src/presentational/TopBar/TopBar.js
+++ src/presentational/TopBar/TopBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Link as RouterLink,
     useLocation
@@ -7,43 +7,70 @@ import {
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import { makeStyles} from '@material-ui/core';
-import './topbar.scss';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import styles from './topbar.module.scss';
 
-const useStyles = makeStyles(theme=>({
-    appbar: {
-        boxShadow: 'none'
-    },
-    title: {
-        fontSize: '20px',
-        fontWeight: '800'
-    },
-    normalTab: {
-        fontSize: '16px',
-        fontWeight: '800'
-    }
-}));
 
 export default ({routes}) => {
     const location = useLocation();
-    const classes = useStyles();
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    const toggleDrawer = open => event => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+          }
+      
+          setDrawerOpen(open);
+    }
     return (
-        <AppBar position="static" className={classes.appbar}>
-            <div className="topbar">
-                <Tabs value={location.pathname}>
-                    {routes
-                    .map((route)=>(
-                        <Tab label={route.label} 
-                            value={route.path} 
-                            to={route.path}
-                            className = {(route.className)? classes[route.className] : null}
-                            component={
-                                React.forwardRef((props, ref)=>
-                                    <RouterLink innerRef={ref} {...props}/>)}/>
-                        ))
-                    }
-                </Tabs>
-            </div>
+        <AppBar position="static" elevation="0" className={styles.topbar}>
+
+                <Hidden smUp>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={toggleDrawer(true)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                </Hidden>
+                <Hidden xsDown>
+                    <Tabs value={location.pathname}>
+                        {routes
+                        .map((route)=>(
+                            <Tab label={route.label} 
+                                value={route.path} 
+                                to={route.path}
+                                className = {(route.className)? styles[route.className] : null}
+                                component={
+                                    React.forwardRef((props, ref)=>
+                                        <RouterLink innerRef={ref} {...props}/>)}/>
+                            ))
+                        }
+                    </Tabs>
+                </Hidden>
+                <Drawer anchor="top" open={drawerOpen} onClose={toggleDrawer(false)}>
+                    <Tabs 
+                        value={location.pathname} 
+                        orientation="vertical"
+                        onClick={toggleDrawer(false)}
+                        onKeyDown={toggleDrawer(false)}>
+                                {routes
+                                .map((route)=>(
+                                    <Tab label={route.label} 
+                                        value={route.path} 
+                                        to={route.path}
+                                        className = {(route.className)? styles[route.className] : null}
+                                        component={
+                                            React.forwardRef((props, ref)=>
+                                                <RouterLink innerRef={ref} {...props}/>)}/>
+                                    ))
+                                }
+                            </Tabs>
+                </Drawer>
+           
         </AppBar>
     );
 };
