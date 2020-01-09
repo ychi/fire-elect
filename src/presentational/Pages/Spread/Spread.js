697aecd8afb0d8f4d45cb0ebf5e83cc9fdf09529
Claire Tsao <amoss.claire@gmail.com>
2020-01-09 22:48:13 +0800

    added charts & articles to spread
    1. updated Kaohsiung map
    2. added charts and articles to Spread.js
    3. modified subtitle style in Spread
    4. roughly arrange page layout

--- src/presentational/Pages/Spread/Spread.js
+++ src/presentational/Pages/Spread/Spread.js
@@ -49,7 +49,7 @@ function legislativeReducer(state, action) {
     ].sort((a, b) => (b.prediction - a.prediction));
 }
 
-  
+
 
 
 
@@ -83,7 +83,7 @@ export default function PeopleVoice({ submittable = true, formContent = null, su
                         </Box>
                     </Hidden>
                 </Grid>
-                
+
                 <Grid item xs={10} md={5}>
                     <Grid container alignItems='flex-end'>
                         <Grid item md={6}>
@@ -108,7 +108,7 @@ export default function PeopleVoice({ submittable = true, formContent = null, su
                         <Typography variant="body3" align="left">
                             <Box>發文不附圖，感覺很模糊</Box>
                             <Box>資訊不分享，全民不幸福</Box>
-                            <Box>十年選舉無人聞,一頁成名天下知！</Box>
+                            <Box>十年選舉無人聞，一頁成名天下知！</Box>
                         </Typography>
                     </Box>
                 </Grid>
@@ -120,7 +120,7 @@ export default function PeopleVoice({ submittable = true, formContent = null, su
                     <Hidden mdUp>
                         <CoundownTimer  fontSize="h6" clockHeight="80vh"/>
                     </Hidden>
-                    <Hidden smDown>   
+                    <Hidden smDown>
                         <CoundownTimer  fontSize="h5" clockHeight="90vh"/>
                     </Hidden>
                 </Grid>
@@ -149,26 +149,63 @@ export default function PeopleVoice({ submittable = true, formContent = null, su
                     <Grid item xs={12} md={12}>
                         <Box height="8vh"></Box>
                     </Grid>
-                    
+
                     <Grid item xs={12} md={12}>
-                        <Box textAlign="left" > 
-                            <Typography component="div"  className={styles.section__brief}>
-                                <Box fontSize="body2.fontSize">不管你是1450、鋼鐵韓粉</Box>
-                                <Box fontSize="body2.fontSize">還是中壢李姓選民</Box>
-                                <Box fontSize="body2.fontSize">又或者以上標籤無法代表你</Box>
-                                <Box fontSize="body2.fontSize">我們好奇你對這次選情的觀察</Box>
-                                <br/>
-                                <Box fontSize="body2.fontSize">右側標籤，各別代表不同族群</Box>
-                                <Box fontSize="body2.fontSize">你覺得哪個族群更傾向支持誰呢？</Box>
-                                <Box fontSize="body2.fontSize">點選標籤分享你的觀察</Box>
-                                <Box fontSize="body2.fontSize" color="#FF2600">每欄最多3個</Box>
-                            </Typography>
-                        </Box>
                     </Grid>
                 </Grid>
                 <Grid container xs={12} md={2}></Grid>
                 <Grid container xs={12} md={8}>
-                    
+                    <Grid item xs={12} md={8} lg={10}>
+                        <Box>
+                            <Typography variant="body3" align="left">
+                                <Box>提起藍綠版圖，「南綠北藍」是普遍印象。誰來自台南，「我感覺你是綠的」，誰出身台北，
+                                知識藍經濟藍的標籤，立刻浮現腦海。但是，地球的板塊都會位移了，沒道理政治的板塊不會變動。
+                                當國家機器動來動去，藍綠支持者究竟是要進來，還是要出去呢？十年藍綠兩茫茫，你的家鄉怎麼動
+                                ，一起來看看。
+                                </Box>
+                            </Typography>
+                        </Box>
+                    </Grid>
+                <Grid container xs={12} md={12} direction="row">
+                    <Box item>
+                        <Typography className={styles.spread__subtitle}>十年平均藍綠得票率差異</Typography>
+                        <Box><img src={require('./img/set1/taipei_map_margin_10y.png')} width="450px" alt="" /></Box>
+                        <img src={require('./img/set1/map_who_win.svg')} alt="" />
+                    </Box>
+                    <Box item>
+                        <Typography className={styles.spread__subtitle}>2008-2018 歷次選舉藍綠得票率差異</Typography>
+                        <img src={require('./img/set1/taipei_margin_10y.png')} width="450px" alt="" />
+                    </Box>
+                </Grid>
+
+                <Grid item xs={12} md={8} lg={10}>
+                    <Box>
+                        <Typography variant="body1" align="left">
+                            <Box>使用方法：</Box>
+                        </Typography>
+                        <Typography variant="body2" align="left">
+                            <Box>
+                            左圖為縣市各村里十年得票率差異平均，藍色即為藍贏，綠色是綠贏，米白色表示差異接近 0。
+                            十年平均可以視為該縣市兩政營支持基準。<br/>
+                            <br/>
+                            右圖為縣市所有村里，十年間七次選舉（縣市長及總統）的兩黨勝差，一條線代表一個村里，線
+                            段顏色來自該村里「前一次」選舉，是藍勝或是綠勝，比如從 2012 到 2014 的線段，會是
+                            2012 該村里的勝利陣營的代表色。<br/>
+                            <br/>
+                            註：藍綠勝利顏色單純依候選人披掛政黨設定，如該年度有無黨籍或其他黨籍候選人參選，如
+                            2014 的台北市，不會被計入藍綠勝負中。<br/>
+                            </Box>
+                        </Typography>
+                        <Typography variant="body3" align="left">
+                            <Box>觀察右圖的七次選舉，兩黨得票率差異，直轄市六都的線段大多平行變動，隨當年大勢同幅
+                            增長或減少，趨勢和幅度都相當一致。然而關注其他縣市的 2012-2018 區段，會發現兩群村里
+                            往不同方向變動的情形，比如雲林 2012-2014 、花蓮 2014-2016、南投 2012-2016。台東
+                            及花蓮的 2012-2014 則是變動走向一致，但有部分村里消長幅度較大的情形。另外，屏東跟高
+                            雄有明顯分成兩群，藍綠村里涇渭分明的情形。
+                            </Box>
+                        </Typography>
+                    </Box>
+                </Grid>
                     <Hidden mdUp>
                         <Grid xs={1}></Grid>
                     </Hidden>
@@ -195,26 +232,101 @@ export default function PeopleVoice({ submittable = true, formContent = null, su
                     <Grid item xs={12} md={12}>
                         <Box height="8vh"></Box>
                     </Grid>
-                    
+
                     <Grid item xs={12} md={12}>
-                        <Box textAlign="left" > 
-                            <Typography component="div"  className={styles.section__brief}>
-                                <Box fontSize="body2.fontSize">不管你是1450、鋼鐵韓粉</Box>
-                                <Box fontSize="body2.fontSize">還是中壢李姓選民</Box>
-                                <Box fontSize="body2.fontSize">又或者以上標籤無法代表你</Box>
-                                <Box fontSize="body2.fontSize">我們好奇你對這次選情的觀察</Box>
-                                <br/>
-                                <Box fontSize="body2.fontSize">右側標籤，各別代表不同族群</Box>
-                                <Box fontSize="body2.fontSize">你覺得哪個族群更傾向支持誰呢？</Box>
-                                <Box fontSize="body2.fontSize">點選標籤分享你的觀察</Box>
-                                <Box fontSize="body2.fontSize" color="#FF2600">每欄最多3個</Box>
-                            </Typography>
-                        </Box>
                     </Grid>
                 </Grid>
-                <Grid container xs={12} md={2}></Grid>
+                <Grid container xs={12} md={2}>
+                </Grid>
                 <Grid container xs={12} md={8}>
-                    
+                    <Typography className={styles.spread__subtitle}>各縣市十年平均藍綠得票率差異</Typography>
+                    <Grid container xs={12} md={12} direction="row">
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/keelung_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>基隆市</Box></Typography>
+                        </Box>
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/newtaipei_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>新北市</Box></Typography>
+                        </Box>
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/taipei_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>臺北市</Box></Typography>
+                        </Box>
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/yilan_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>宜蘭縣</Box></Typography>
+                        </Box>
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/taoyuan_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>桃園市</Box></Typography>
+                        </Box>
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/hsinchucounty_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>新竹縣</Box></Typography>
+                        </Box>
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/hsinchucity_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>新竹市</Box></Typography>
+                        </Box>
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/miaoli_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>苗栗縣</Box></Typography>
+                        </Box>
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/taichung_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>臺中市</Box></Typography>
+                        </Box>
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/changhua_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>彰化縣</Box></Typography>
+                        </Box>
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/nantou_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>南投縣</Box></Typography>
+                        </Box>
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/jiayicity_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>嘉義市</Box></Typography>
+                        </Box>
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/jiayicounty_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>嘉義縣</Box></Typography>
+                        </Box>
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/tainan_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>臺南市</Box></Typography>
+                        </Box>
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/kaohsiung_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>高雄市</Box></Typography>
+                        </Box>
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/pingtung_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>屏東縣</Box></Typography>
+                        </Box>
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/hualian_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>花蓮縣</Box></Typography>
+                        </Box>
+                        <Box item height="150px">
+                            <Box height="120px"><img src={require('./img/set1/taitung_map_margin_10y.png')} width="150px" alt="" /></Box>
+                            <Typography variant="body4" align="left"><Box>臺東縣</Box></Typography>
+                        </Box>
+                    </Grid>
+                <Grid item xs={12} md={8} lg={10}>
+                    <Typography variant="body3" align="left">
+                        <Box>鍵盤繞境台灣，會發現各縣市的藍綠區塊，頗有一種漸層質感。藍色的相鄰區塊，大致是藍的，
+                        綠色的鄰居，大多也是綠的，藍綠相接處，常有接近 0 的米白色，或至少淺藍淺綠作為緩衝。深藍綠
+                        短兵相接處雖有，但數量稀少。
+                        </Box>
+                        <Box>大部分的縣市，藍綠壁壘分明，唯有基隆，全區夢幻粉彩，輕飄飄跨越藍綠之間，來去自如。藍
+                        綠除了開戰濁水溪南北之外，部分包含山區的縣市，也有平地區域較綠、而大面積的山地區塊較藍的情
+                        形。
+                        </Box>
+                    </Typography>
+                </Grid>
+
                     <Hidden mdUp>
                         <Grid xs={1}></Grid>
                     </Hidden>
@@ -241,26 +353,25 @@ export default function PeopleVoice({ submittable = true, formContent = null, su
                     <Grid item xs={12} md={12}>
                         <Box height="8vh"></Box>
                     </Grid>
-                    
+
                     <Grid item xs={12} md={12}>
-                        <Box textAlign="left" > 
-                            <Typography component="div"  className={styles.section__brief}>
-                                <Box fontSize="body2.fontSize">不管你是1450、鋼鐵韓粉</Box>
-                                <Box fontSize="body2.fontSize">還是中壢李姓選民</Box>
-                                <Box fontSize="body2.fontSize">又或者以上標籤無法代表你</Box>
-                                <Box fontSize="body2.fontSize">我們好奇你對這次選情的觀察</Box>
-                                <br/>
-                                <Box fontSize="body2.fontSize">右側標籤，各別代表不同族群</Box>
-                                <Box fontSize="body2.fontSize">你覺得哪個族群更傾向支持誰呢？</Box>
-                                <Box fontSize="body2.fontSize">點選標籤分享你的觀察</Box>
-                                <Box fontSize="body2.fontSize" color="#FF2600">每欄最多3個</Box>
-                            </Typography>
-                        </Box>
                     </Grid>
                 </Grid>
                 <Grid container xs={12} md={2}></Grid>
                 <Grid container xs={12} md={8}>
-                    
+                    <Grid item xs={12} md={8} lg={10}>
+                        <Typography variant="body3" align="left">
+                            <Box>我們將最國民黨支持度最高的前 10% 村里抓出來，標記為鐵桿藍營區，以回歸模型探索，是否
+                            有哪些人口特徵，與高度支持藍營相關。模型發現這些藍營區，大體男性人口較多，且多為已婚男性多
+                            的區域。民進黨支持度最高的區域，則大致呈現相反的趨勢。
+                            </Box>
+                            <Box>改從人口特徵出發，拉出擁有大學學歷的人口比例最多，跟最少的 10% 村里，檢視這兩群村里
+                            的藍綠得票率差異，會發現教育程度較高的這一群村里，相較於教育程度較低的那群，兩黨的勝差更小
+                            ，也有整體轉藍或者轉綠的情況，可能對政黨的忠誠度較低。而大學學歷人口比例最少的村里群，雖然
+                            也有部分變天，但數量較少，單一村里開出的票，對特定政黨的支持較穩定。
+                            </Box>
+                        </Typography>
+                    </Grid>
                     <Hidden mdUp>
                         <Grid xs={1}></Grid>
                     </Hidden>
@@ -282,7 +393,7 @@ export default function PeopleVoice({ submittable = true, formContent = null, su
                             <Typography className={styles.bottom__linkIntro__text} fontSize="body2.fontSize">
                                 <Box>局勢怎麼走</Box>
                                 <Box>是當局者迷還是旁觀者清</Box>
-                                <br/>   
+                                <br/>
                                 <Box>選前預測不封關</Box>
                                 <Box>把握倒數幾天</Box>
                                 <Box marginTop="12px" marginBottom="24px">看看你能否鐵口直斷</Box>
