8177b43f5cbdf6e1dfd28b92be66737c56f722d9
Yen-Chi Chen <yen.chen@booking.com>
2020-01-10 22:12:52 +0100

    CongressSummary

--- src/container/CongressSummaryContainer.js
+++ src/container/CongressSummaryContainer.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import {TileMap, TileStyles} from '../presentational/Components/PredictSeats/Tiles';
 import {distributionToSeats} from '../logicUtils/PartyUtil';
+import FirebaseContext from '../tools/firebase/Context';
 export default ()=>{
-    const seats = distributionToSeats([]);
+    const firebase = useContext(FirebaseContext);
+    const [seats, setSeats] = useState(distributionToSeats([]));
+    useEffect(()=>{
+        const unsub = firebase.db.collection('summary').doc('congress')
+        .onSnapshot(snapshot => {
+            const result = snapshot.data();
+            let dist = [];
+            for (let k in result) {
+                dist.push({
+                    partyId: k==='independent'?'other' : k,
+                    prediction: result[k]
+                })
+            }
+            const seats =distributionToSeats(dist);
+            setSeats(seats);
+        });
+        return unsub;
+    }, [firebase]);
+    
     return (
     <div style={{padding: '5px 0'}}>
         <TileMap seats={seats} tileClass={TileStyles.xs}/>
