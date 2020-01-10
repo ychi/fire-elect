2fb0cd4b519fe9953d7df0adce660cd108c55073
imyentsen <imyentsen@gmail.com>
2020-01-11 06:57:45 +0800

    line graph

--- src/presentational/Components/DistParliament/DistParliament.js
+++ src/presentational/Components/DistParliament/DistParliament.js
@@ -1,10 +1,62 @@
 import React from 'react';
 import './distparliament.scss';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
+import { render } from 'react-dom';
+import Highcharts from 'highcharts/highstock';
+import HighchartsReact from 'highcharts-react-official';
+
+const bar = {
+  chart: {
+    height: 50,
+    type: 'bar'
+},
+title: {
+    text: ''
+},
+credits: {
+  enabled: false
+},
+xAxis: {
+    visible: false,
+},
+yAxis: {
+    visible: false,
+},
+legend: {
+    enabled: false,
+},
+
+plotOptions: {
+    series: {
+        stacking: 'normal',
+        pointWidth: 10
+    },
+    bar: {
+        dataLabels: {
+            enabled: true,
+            distance : -50,
+            formatter: function() {
+                var dlabel = this.series.name + '<br/>';
+                dlabel += Math.round(this.percentage*100)/100 + ' %';
+                    return dlabel
+             },
+            style: {
+                color: 'white',
+            },
+        },
+        
+    },
+},
+series: [{
+    name: '吳宜農',
+    data: [64]
+}, {
+    name: '蔣萬安',
+    data: [36]
+}]
+};
 
 function DistParliament(props) {
   return (
@@ -22,7 +74,14 @@ function DistParliament(props) {
           <span className="losecount">{props.losecount}</span>
         </Grid>
       </Grid>  
-      <Grid item xs={12} className="predict_bar"></Grid>
+      <Grid item xs={12} className="predict_bar">
+        <Box className="barwidth">
+          <HighchartsReact
+        highcharts={Highcharts}
+        options={bar}
+        />
+        </Box>
+      </Grid>
     </Grid>
   );
 }
