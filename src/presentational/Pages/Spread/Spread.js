0bbd4d1d386dd582ec91847f708b0685e84b61fb
Claire Tsao <amoss.claire@gmail.com>
2020-01-10 09:06:04 +0800

    Spread static elements ok
    1. added iron imgs
    2. rearrange the layout
    3. added open data part

--- src/presentational/Pages/Spread/Spread.js
+++ src/presentational/Pages/Spread/Spread.js
@@ -1,4 +1,5 @@
-import React,{ useReducer } from 'react';
+import React, { useReducer } from 'react';
+import ReactDOM from 'react-dom';
 import styles from './spread.module.scss';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
@@ -158,10 +159,7 @@ export default function PeopleVoice({ submittable = true, formContent = null, su
                     <Grid item xs={12} md={8} lg={10}>
                         <Box>
                             <Typography variant="body3" align="left">
-                                <Box>提起藍綠版圖，「南綠北藍」是普遍印象。誰來自台南，「我感覺你是綠的」，誰出身台北，
-                                知識藍經濟藍的標籤，立刻浮現腦海。但是，地球的板塊都會位移了，沒道理政治的板塊不會變動。
-                                當國家機器動來動去，藍綠支持者究竟是要進來，還是要出去呢？十年藍綠兩茫茫，你的家鄉怎麼動
-                                ，一起來看看。
+                                <Box>提起藍綠版圖，「南綠北藍」是普遍印象。誰來自台南，「我感覺你是綠的」，誰出身台北，知識藍經濟藍的標籤，立刻浮現腦海。但是，地球的板塊都會位移了，沒道理政治的板塊不會變動。當國家機器動來動去，藍綠支持者究竟是要進來，還是要出去呢？十年藍綠兩茫茫，你的家鄉怎麼動，一起來看看。
                                 </Box>
                             </Typography>
                         </Box>
@@ -181,27 +179,20 @@ export default function PeopleVoice({ submittable = true, formContent = null, su
                 <Grid item xs={12} md={8} lg={10}>
                     <Box>
                         <Typography variant="body1" align="left">
-                            <Box>使用方法：</Box>
+                            <Box mt={2}>使用方法：</Box>
                         </Typography>
                         <Typography variant="body2" align="left">
                             <Box>
-                            左圖為縣市各村里十年得票率差異平均，藍色即為藍贏，綠色是綠贏，米白色表示差異接近 0。
-                            十年平均可以視為該縣市兩政營支持基準。<br/>
+                            左圖為縣市各村里十年得票率差異平均，藍色即為藍贏，綠色是綠贏，米白色表示差異接近 0。十年平均可以視為該縣市兩政營支持基準。<br/>
                             <br/>
-                            右圖為縣市所有村里，十年間七次選舉（縣市長及總統）的兩黨勝差，一條線代表一個村里，線
-                            段顏色來自該村里「前一次」選舉，是藍勝或是綠勝，比如從 2012 到 2014 的線段，會是
-                            2012 該村里的勝利陣營的代表色。<br/>
+                            右圖為縣市所有村里，十年間七次選舉（縣市長及總統）的兩黨勝差，一條線代表一個村里，線段顏色來自該村里「前一次」選舉，是藍勝或是綠勝，比如從 2012 到 2014 的線段，會是 2012 該村里的勝利陣營的代表色。<br/>
                             <br/>
-                            註：藍綠勝利顏色單純依候選人披掛政黨設定，如該年度有無黨籍或其他黨籍候選人參選，如
-                            2014 的台北市，不會被計入藍綠勝負中。<br/>
+                            註：藍綠勝利顏色單純依候選人披掛政黨設定，如該年度有無黨籍或其他黨籍候選人參選，如 2014 的台北市，不會被計入藍綠勝負中。<br/>
                             </Box>
                         </Typography>
+                        <Typography className={styles.spread__subtitle}>寶傑，十年選舉路，你怎麼看</Typography>
                         <Typography variant="body3" align="left">
-                            <Box>觀察右圖的七次選舉，兩黨得票率差異，直轄市六都的線段大多平行變動，隨當年大勢同幅
-                            增長或減少，趨勢和幅度都相當一致。然而關注其他縣市的 2012-2018 區段，會發現兩群村里
-                            往不同方向變動的情形，比如雲林 2012-2014 、花蓮 2014-2016、南投 2012-2016。台東
-                            及花蓮的 2012-2014 則是變動走向一致，但有部分村里消長幅度較大的情形。另外，屏東跟高
-                            雄有明顯分成兩群，藍綠村里涇渭分明的情形。
+                            <Box mt={2}>觀察右圖的七次選舉，兩黨得票率差異，直轄市六都的線段大多平行變動，隨當年大勢同幅增長或減少，趨勢和幅度都相當一致。然而關注其他縣市的 2012-2018 區段，會發現兩群村里往不同方向變動的情形，比如雲林 2012-2014 、花蓮 2014-2016、南投 2012-2016。台東及花蓮的 2012-2014 則是變動走向一致，但有部分村里消長幅度較大的情形。另外，屏東跟高雄有明顯分成兩群，藍綠村里涇渭分明的情形。
                             </Box>
                         </Typography>
                     </Box>
@@ -240,7 +231,7 @@ export default function PeopleVoice({ submittable = true, formContent = null, su
                 </Grid>
                 <Grid container xs={12} md={8}>
                     <Typography className={styles.spread__subtitle}>各縣市十年平均藍綠得票率差異</Typography>
-                    <Grid container xs={12} md={12} direction="row">
+                    <Grid container xs={12} md={8} lg={10} mb={2} direction="row">
                         <Box item height="150px">
                             <Box height="120px"><img src={require('./img/set1/keelung_map_margin_10y.png')} width="150px" alt="" /></Box>
                             <Typography variant="body4" align="left"><Box>基隆市</Box></Typography>
@@ -315,14 +306,11 @@ export default function PeopleVoice({ submittable = true, formContent = null, su
                         </Box>
                     </Grid>
                 <Grid item xs={12} md={8} lg={10}>
+                    <Typography className={styles.spread__subtitle}>齁，我的鄰居都是 X 粉啦</Typography>
                     <Typography variant="body3" align="left">
-                        <Box>鍵盤繞境台灣，會發現各縣市的藍綠區塊，頗有一種漸層質感。藍色的相鄰區塊，大致是藍的，
-                        綠色的鄰居，大多也是綠的，藍綠相接處，常有接近 0 的米白色，或至少淺藍淺綠作為緩衝。深藍綠
-                        短兵相接處雖有，但數量稀少。
+                        <Box mt={2}>鍵盤繞境台灣，會發現各縣市的藍綠區塊，頗有一種漸層質感。藍色的相鄰區塊，大致是藍的，綠色的鄰居，大多也是綠的，藍綠相接處，常有接近 0 的米白色，或至少淺藍淺綠作為緩衝。深藍綠短兵相接處雖有，但數量稀少。
                         </Box>
-                        <Box>大部分的縣市，藍綠壁壘分明，唯有基隆，全區夢幻粉彩，輕飄飄跨越藍綠之間，來去自如。藍
-                        綠除了開戰濁水溪南北之外，部分包含山區的縣市，也有平地區域較綠、而大面積的山地區塊較藍的情
-                        形。
+                        <Box mt={2}>大部分的縣市，藍綠壁壘分明，唯有基隆，全區夢幻粉彩，輕飄飄跨越藍綠之間，來去自如。藍綠除了開戰濁水溪南北之外，部分包含山區的縣市，也有平地區域較綠、而大面積的山地區塊較藍的情形。
                         </Box>
                     </Typography>
                 </Grid>
@@ -360,15 +348,62 @@ export default function PeopleVoice({ submittable = true, formContent = null, su
                 <Grid container xs={12} md={2}></Grid>
                 <Grid container xs={12} md={8}>
                     <Grid item xs={12} md={8} lg={10}>
+                        <Typography className={styles.spread__subtitle}>鐵藍區長什麼樣子？</Typography>
                         <Typography variant="body3" align="left">
-                            <Box>我們將最國民黨支持度最高的前 10% 村里抓出來，標記為鐵桿藍營區，以回歸模型探索，是否
-                            有哪些人口特徵，與高度支持藍營相關。模型發現這些藍營區，大體男性人口較多，且多為已婚男性多
-                            的區域。民進黨支持度最高的區域，則大致呈現相反的趨勢。
+                            <Box mt={2}>我們將最國民黨支持度最高的前 10% 村里抓出來，標記為鐵桿藍營區，以回歸模型探索，是否有哪些人口特徵，與高度支持藍營相關。模型發現這些藍營區，大體男性人口較多，且多為已婚男性多的區域。民進黨支持度最高的區域，則大致呈現相反的趨勢。
                             </Box>
-                            <Box>改從人口特徵出發，拉出擁有大學學歷的人口比例最多，跟最少的 10% 村里，檢視這兩群村里
-                            的藍綠得票率差異，會發現教育程度較高的這一群村里，相較於教育程度較低的那群，兩黨的勝差更小
-                            ，也有整體轉藍或者轉綠的情況，可能對政黨的忠誠度較低。而大學學歷人口比例最少的村里群，雖然
-                            也有部分變天，但數量較少，單一村里開出的票，對特定政黨的支持較穩定。
+                        </Typography>
+                        <Typography className={styles.spread__subtitle}>十年藍綠得票率差異</Typography>
+                        <Box><img src={require('./img/set2/margin_y.svg')} width="100%" alt="" /></Box>
+                        <Typography className={styles.spread__subtitle}>上大學會怎樣？</Typography>
+                        <Typography variant="body3" align="left">
+                            <Box mt={3}>改從人口特徵出發，拉出擁有大學學歷的人口比例最多，跟最少的 10% 村里，檢視這兩群村里的藍綠得票率差異，會發現教育程度較高的這一群村里，相較於教育程度較低的那群，兩黨的勝差更小，也有整體轉藍或者轉綠的情況，可能對政黨的忠誠度較低。而大學學歷人口比例最少的村里群，雖然也有部分變天，但數量較少，單一村里開出的票，對特定政黨的支持較穩定。
+                            </Box>
+                        </Typography>
+                    </Grid>
+                    <Hidden mdUp>
+                        <Grid xs={1}></Grid>
+                    </Hidden>
+                    <Grid md={1}></Grid>
+                </Grid>
+            </Grid>
+        </section>
+
+        <section className={styles.window__section}>
+            <Grid container>
+                <Grid xs={1} md={1}></Grid>
+                <Grid container xs={10} md={1}>
+                    <Grid item xs={12} md={12}>
+                        <Box width={50} height={50} display="inline-block" textAlign="center">
+                            <img src={require('./section_pattern.svg')} alt="" width="200%" />
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                        <Box>
+                            <Typography variant="h2" className={styles.predict__subtitle__vertical} display="inline">資料在<br/>哪裡</Typography>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                        <Box height="8vh"></Box>
+                    </Grid>
+
+                    <Grid item xs={12} md={12}>
+                    </Grid>
+                </Grid>
+                <Grid container xs={12} md={2}></Grid>
+                <Grid container xs={12} md={8}>
+                    <Grid item xs={12} md={8} lg={10}>
+                        <Typography variant="body3" align="left">
+                            <Box mt={2}>本篇圖表所使用的，加上村里代碼的 2008-2018 選舉資料
+                                <a href="https://github.com/missmoss/expires-next-day-2020-data" target="_blank"  className={styles.spread__link}>
+                                    在此
+                                </a>
+                                ，請隨意夾去配
+                            </Box>
+                        </Typography>
+                        <Typography className={styles.spread__subtitle}>特別感謝</Typography>
+                        <Typography variant="body3" align="left">
+                            <Box mt={2}>Kirby Wu 的 <a href="https://plotdb.com/" target="_blank" className={styles.spread__link}>Plotdb</a> 讓畫地圖變得容易。室友 Po & Maureen 幫忙救火。
                             </Box>
                         </Typography>
                     </Grid>
