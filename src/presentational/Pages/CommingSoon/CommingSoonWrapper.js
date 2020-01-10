f75873ff33f416a13879517f47fb736e2a9f09c1
Yen-Chi Chen <yen.chen@booking.com>
2020-01-10 17:22:32 +0100

    developer flag

--- src/presentational/Pages/CommingSoon/CommingSoonWrapper.js
+++ src/presentational/Pages/CommingSoon/CommingSoonWrapper.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import CommingSoon from '../CommingSoon/CommingSoon';
+import {
+    useLocation
+  } from "react-router-dom";
+
+  function useQuery() {
+    return new URLSearchParams(useLocation().search);
+  }  
 
 export default ({ready, renderComp})=>{
-    return ready ? renderComp() : <CommingSoon/>;
+    let q = useQuery();
+    let dev = q.get('developing');
+    return ready|| dev ? renderComp() : <CommingSoon/>;
 };
