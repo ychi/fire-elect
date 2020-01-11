f096bcd5e838378ece8f4abadb2cbff8388a4741
I-Chiao Hsu <ichiao.hsu@gmail.com>
2020-01-11 12:31:10 +0800

    feat(Warroom): connect actual data in CongressSummaryContainer

--- src/container/CongressSummaryContainer.js
+++ src/container/CongressSummaryContainer.js
@@ -6,19 +6,16 @@ export default ()=>{
     const firebase = useContext(FirebaseContext);
     const [seats, setSeats] = useState(distributionToSeats([]));
     useEffect(()=>{
-        const unsub = firebase.db.collection('summary').doc('congress')
-        .onSnapshot(snapshot => {
-            const result = snapshot.data();
-            let dist = [];
-            for (let k in result) {
-                dist.push({
-                    partyId: k==='independent'?'other' : k,
-                    prediction: result[k]
-                })
-            }
-            const seats =distributionToSeats(dist);
+        const unsub = firebase.db.collection('summary_congress')
+          .onSnapshot(snapshot => {
+            const dist = snapshot.docs.map(d => ({
+              partyId: d.id === 'independent' ? 'other' : d.id,
+              prediction: d.data().data.total_confirmed
+            }))
+            const seats = distributionToSeats(dist);
             setSeats(seats);
-        });
+          });
+
         return unsub;
     }, [firebase]);
     
